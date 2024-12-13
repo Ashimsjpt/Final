cbbd244cdf99960d47382a27a865730adae6bed3
Ashim Sijapati <177884190+Ashimsjpt@users.noreply.github.com>
2024-12-13 14:41:25 -0500

    uploads picture sucessfully.

--- app.py
+++ app.py
@@ -5,9 +5,16 @@ app = Flask(__name__)
 UPLOAD_FOLDER = './static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        print("POST request received")
+        file = request.files.get('image')
+        if file:
+            print(f"File received: {file.filename}")
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(filepath)
+            return f"File uploaded successfully: {file.filename}"
+        else:
+            print("No file found in the request")
     return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
